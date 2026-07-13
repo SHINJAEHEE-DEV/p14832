f5a949e3ba2f206bd2f67c544832c0a11dad2653
SHINJAEHEE-DEV <asjh011969@gmail.com>
2026-07-13 11:29:15 +0900

    variable.tf 추가

--- infra/main.tf
+++ infra/main.tf
@@ -9,7 +9,7 @@ terraform {
 
 # AWS 설정 시작
 provider "aws" {
-  region = "ap-northeast-2"
+  region = var.region
 }
 # AWS 설정 끝
 
@@ -21,6 +21,17 @@ resource "aws_vpc" "vpc_1" {
   enable_dns_hostnames = true
 
   tags = {
-    Name = "terra-vpc-1"
+    Name = "${var.prefix}-vpc-1"
+  }
+}
+
+resource "aws_vpc" "vpc_2" {
+  cidr_block = "10.0.0.0/16"
+
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "${var.prefix}-vpc-2"
   }
 }
